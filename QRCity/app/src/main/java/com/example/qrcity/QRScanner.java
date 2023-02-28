66131eadf2e077e981d9d4cf123b2e1bef8bc247
Derek <derekf711@gmail.com>
2023-02-28 11:58:47 -0700

    Refactored Second Fragment
    - Renamed starting second fragment class and references to QRScanner
    - Renamed fragment_second layout and references to fragment_code_scanner
    - Redesigned code scanner layout to include the code scanner view and a cancel button

--- QRCity/app/src/main/java/com/example/qrcity/QRScanner.java
+++ QRCity/app/src/main/java/com/example/qrcity/QRScanner.java
@@ -9,11 +9,11 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.qrcity.databinding.FragmentSecondBinding;
+import com.example.qrcity.databinding.FragmentCodeScannerBinding;
 
-public class SecondFragment extends Fragment {
+public class QRScanner extends Fragment {
 
-    private FragmentSecondBinding binding;
+    private FragmentCodeScannerBinding binding;
 
     @Override
     public View onCreateView(
@@ -21,7 +21,7 @@ public class SecondFragment extends Fragment {
             Bundle savedInstanceState
     ) {
 
-        binding = FragmentSecondBinding.inflate(inflater, container, false);
+        binding = FragmentCodeScannerBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
     }
@@ -32,8 +32,8 @@ public class SecondFragment extends Fragment {
         binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+                NavHostFragment.findNavController(QRScanner.this)
+                        .navigate(R.id.action_CodeScannerFragment_to_FirstFragment);
             }
         });
     }
