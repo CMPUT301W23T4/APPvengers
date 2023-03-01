226a656a0b61a10699d9899c0c74d4ed07bb86f7
Derek <derekf711@gmail.com>
2023-03-01 13:04:21 -0700

    Finished ScoringSystem
    - Completed ScoringSystem.getScore(String hash)
    - TODO: Ask if we should add +1 to the score for single zero's

--- QRCity/app/src/main/java/com/example/qrcity/ScoringSystem.java
+++ QRCity/app/src/main/java/com/example/qrcity/ScoringSystem.java
@@ -1,7 +1,5 @@
 package com.example.qrcity;
 
-// WIP
-
 public class ScoringSystem {
 
     public int getScore(String hash){
@@ -10,87 +8,60 @@ public class ScoringSystem {
         int score = 0;
         int count = 1;
 
-        char Current;
-        char Previous;
+        int current;
+        int previous = 0;
 
-        boolean addScore;
+        boolean calcScore;
 
-        for (int i=1; i<64; i++){
+        //Cycle through all elements in the char array
+        for (int i=0; i<64; i++){
 
-            //Get the current and previous characters in the array
-            Current = charArray[i];
-            Previous = charArray[i-1];
-            addScore = false;
+            calcScore = false;
 
-            //Check if we should calculate the score for a number streak
-            if (Current != Previous){   //Streak is broken
-                addScore = true;
-            } else {
-                count++;
-                if (i == 63){   //Reached the last element
-                    addScore = true;
+            //Get point value of the current character
+            current = charArray[i];
+
+            if (current <=57){
+                current -= 48;
+            }
+            else{
+                current -= 87;
+            }
+
+            if (current == 0){
+                current = 20;
+            }
+
+            //count elements
+            if (i == 0){
+                previous = current;
+                continue;
+            }
+            else{
+                if (current != previous || i == 63){
+                    calcScore = true;
+                }
+                else{
+                    count++;
                 }
             }
 
-            //calculate the score for a number streak
-            if (addScore) {
-                if (count>1){   //Streak is larger than 1
-                    switch (Previous){
-                        case '0':
-                            score += Math.pow(20, (count-1));
-                            break;
-                        case '1':
-                            score += Math.pow(1, (count-1));
-                            break;
-                        case '2':
-                            score += Math.pow(2, (count-1));
-                            break;
-                        case '3':
-                            score += Math.pow(3, (count-1));
-                            break;
-                        case '4':
-                            score += Math.pow(4, (count-1));
-                            break;
-                        case '5':
-                            score += Math.pow(5, (count-1));
-                            break;
-                        case '6':
-                            score += Math.pow(6, (count-1));
-                            break;
-                        case '7':
-                            score += Math.pow(7, (count-1));
-                            break;
-                        case '8':
-                            score += Math.pow(8, (count-1));
-                            break;
-                        case '9':
-                            score += Math.pow(9, (count-1));
-                            break;
-                        case 'a':
-                            score += Math.pow(10, (count-1));
-                            break;
-                        case 'b':
-                            score += Math.pow(11, (count-1));
-                            break;
-                        case 'c':
-                            score += Math.pow(12, (count-1));
-                            break;
-                        case 'd':
-                            score += Math.pow(13, (count-1));
-                            break;
-                        case 'e':
-                            score += Math.pow(14, (count-1));
-                            break;
-                        case 'f':
-                            score += Math.pow(15, (count-1));
-                            break;
-                    }
-                } else if (Previous == '0') {   //The previous value was a 0
-                    score++;
-                }
-                count = 0;
-            }
+            //calculate score addition
+            if (calcScore){
+                int add = 0;
 
+                /*if (count == 1 && previous == 20) {
+                    add = (int)(Math.pow(previous, count-1));
+                }
+                else*/ if (count > 1) {
+                    add = (int)(Math.pow(previous, count-1));
+                }
+
+                score += add;
+
+                previous = current;
+                count = 1;
+            }
         }
 
         return score;
