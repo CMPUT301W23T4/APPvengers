d9bc2bb79e42204bd75629a9920b25c63e720263
banana <wangshu@ualberta.ca>
2023-03-13 13:56:47 -0600

    change

---
+++ QRCity/app/src/main/java/com/example/qrcity/Removeprofile.java
@@ -0,0 +1,57 @@
+package com.example.qrcity;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class Removeprofile extends DialogFragment {
+    private OnFragmentInteractionListener listener;
+
+    public interface OnFragmentInteractionListener {
+        void onRemoveOKPressed();
+    }
+    @Override
+    public void onAttach(Context context){
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener)
+        {
+            listener = (OnFragmentInteractionListener) context;
+        }
+        else
+        {
+            throw new RuntimeException(context.toString()
+                    +"must implement OnFragmentInteractionListener");
+        }
+    }
+    @Override
+    @NonNull
+    public Dialog onCreateDialog(@NonNull Bundle savedInstanceState){
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.remove_profile,null);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        return builder
+                .setView(view)
+                .setTitle("request remove user profile?")
+                .setNegativeButton("Cancel",null)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        listener.onRemoveOKPressed();
+                    }
+                }).create();
+
+    }
+    public Removeprofile(){}
+}
