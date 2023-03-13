301d2860965fb3e2e816a5c21349b68dd8d253c7
banana <wangshu@ualberta.ca>
2023-03-13 14:54:38 -0600

    change location button

--- QRCity/app/src/main/java/com/example/qrcity/FirstFragment.java
+++ QRCity/app/src/main/java/com/example/qrcity/FirstFragment.java
@@ -1,5 +1,6 @@
 package com.example.qrcity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -43,6 +44,15 @@ public class FirstFragment extends Fragment {
             public void onClick(View view) {
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_CodeListview);
+            }
+        });
+
+        binding.getRoot().findViewById(R.id.Location).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Clicked location");
+                Intent intent = new Intent();
+                startActivity(intent);
             }
         });
     }
