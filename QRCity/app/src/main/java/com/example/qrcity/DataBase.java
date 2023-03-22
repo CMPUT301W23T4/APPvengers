de001b403e082f86179e0a122896cbaecfdb97f0
banana <wangshu@ualberta.ca>
2023-03-22 16:41:35 -0600

    update on database

--- QRCity/app/src/main/java/com/example/qrcity/DataBase.java
+++ QRCity/app/src/main/java/com/example/qrcity/DataBase.java
@@ -2,13 +2,19 @@ package com.example.qrcity;
 
 import android.graphics.Bitmap;
 import android.util.Base64;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
@@ -20,10 +26,18 @@ public class DataBase {
     private static DataBase instance;
     final private FirebaseFirestore db;
     final private FirebaseStorage storage;
+    CollectionReference collectionReference;
+    CollectionReference ownerCollection;
+    StorageReference photoColletion;
+    final String TAG = "what to put here";
+
 
     private DataBase() {
         db = FirebaseFirestore.getInstance();
         storage = FirebaseStorage.getInstance();
+        collectionReference = db.collection("Users");
+        ownerCollection = db.collection("Owners");
+        photoColletion = storage.getReference();
     }
 
     public static DataBase getInstance() {
@@ -33,37 +47,36 @@ public class DataBase {
         return instance;
     }
 
-    public void addUser(User user){
+    public void addUser(User user) {
         // Collection reference
         CollectionReference cr = db.collection("users");
 
-        Map<String,Object> data = new HashMap<>();
-        data.put("name",user.getName());
-        data.put("contactinfo",user.getContactInfo());
-        data.put("totalscore",user.getTotalScore());
+        Map<String, Object> data = new HashMap<>();
+        data.put("name", user.getName());
+        data.put("contactinfo", user.getContactInfo());
+        data.put("totalscore", user.getTotalScore());
         data.put("numcodes", user.getNumCodes());
         cr.document(user.getUserId()).set(data);
     }
 
-    public void getUser(String userId, OnGetUserListener listener){
+    public void getUser(String userId, OnGetUserListener listener) {
         CollectionReference cr = db.collection("users");
         DocumentReference dr = cr.document(userId);
         dr.get().addOnCompleteListener(task -> {
-            if (task.isSuccessful()){
+            if (task.isSuccessful()) {
                 User user = new User();
                 DocumentSnapshot doc = task.getResult();
-                if(doc.exists()){
-                    String name= (String) doc.getData().get("name");
-                    String contactInfo= (String) doc.getData().get("contactinfo");
-                    long totalScore= (long) doc.getData().get("totalscore");
+                if (doc.exists()) {
+                    String name = (String) doc.getData().get("name");
+                    String contactInfo = (String) doc.getData().get("contactinfo");
+                    long totalScore = (long) doc.getData().get("totalscore");
                     long numCodes = (long) doc.getData().get("numcodes");
                     user.setId(userId);
                     user.setName(name);
                     user.setContactInfo(contactInfo);
                     user.set_Total_Score(totalScore);
                     user.set_Num_Codes(numCodes);
-                }
-                else{
+                } else {
                     user = null;
                 }
                 listener.getUserListener(user);
@@ -72,12 +85,12 @@ public class DataBase {
 
     }
 
-    public void getUsers(OnGetUsersListener listener){
+    public void getUsers(OnGetUsersListener listener) {
         CollectionReference cr = db.collection("users");
         cr.get().addOnCompleteListener(task -> {
             ArrayList<String> userIds = new ArrayList<>();
-            if(task.isSuccessful()){
-                for(QueryDocumentSnapshot doc: task.getResult()){
+            if (task.isSuccessful()) {
+                for (QueryDocumentSnapshot doc : task.getResult()) {
                     String userid = doc.getId();
                     userIds.add(userid);
                 }
@@ -86,14 +99,14 @@ public class DataBase {
         });
     }
 
-    public void addCode(ScannableCode code, String hash){
+    public void addCode(ScannableCode code, String hash) {
         CollectionReference cr = db.collection("codes");
         DocumentReference dr = cr.document(hash);
         Map<String, Object> data = new HashMap<>();
-        data.put("score",code.getScore());
-        data.put("comment",code.getComment());
-        data.put("location",code.getLocation());
-        data.put("name",code.getName());
+        data.put("score", code.getScore());
+        data.put("comment", code.getComment());
+        data.put("location", code.getLocation());
+        data.put("name", code.getName());
         if (code.getPhoto() == null) {
             data.put("photo", null);
         } else {
@@ -110,4 +123,71 @@ public class DataBase {
 
         dr.set(data);
     }
+
+    public void removerUserData(User user) {//removes User data from the firebase
+        collectionReference
+                .document(user.getUserId())
+                .delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        //if data is successfully uploaded
+                        Log.d(TAG, "User " + user.getUserId() + "  successfully deleted");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //if data upload fails
+                        Log.d(TAG, "User " + user.getUserId() + " data failed to delete: " + e.toString());
+                    }
+                });
+    }
+    public void addOwner(String OwnerName, String name) {
+        Map<String, Object> ownerData = new HashMap<>();
+        ownerData.put("name", name);
+        ownerCollection
+                .document(OwnerName)
+                .set(ownerData)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        //if data is successfully uploaded
+                        Log.d(TAG, "Code " + OwnerName + "  successfully uploaded");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //if data upload fails
+                        Log.d(TAG, "User " + OwnerName + " data failed to upload: " + e.toString());
+                    }
+                });
+    }
+    public void removeOwner(String OwnerName) {
+        ownerCollection
+                .document(OwnerName)
+                .delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        //if data is successfully uploaded
+                        Log.d(TAG, "User " + OwnerName + "  successfully deleted");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //if data upload fails
+                        Log.d(TAG, "User " + OwnerName + " data failed to delete: " + e.toString());
+                    }
+                });
+    }
+    public void addUsers(ArrayList<User> users) {
+        for (User user : users) {
+            addUser(user);
+        }
+    }
+
+
 }
